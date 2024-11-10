1f24cd4412534df7b578bc506097668ebe3d31ed
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-11-10 17:05:29 +0100

    Add Counter.jsx component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 import { useState } from 'react'
 import './App.css'
 
@@ -16,6 +17,7 @@ function App() {
         <MainContent />
         <WelcomeMessage />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Counter />
         <Footer />
       </div>
       
