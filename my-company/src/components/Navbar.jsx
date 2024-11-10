349ae2500922c84386e7697012e64a7d2f487b7b
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-11-10 18:52:53 +0100

    Add display and justify-content styling to Narbar.jsx in my-company app

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+        <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff', display: 'flex', justifyContent: 'space-evenly' }}>
             <Link to="/" style={{ color: 'white', marginRight: '10px' }}>Home</Link>
             <Link to="/about" style={{ color: 'white', marginRight: '10px' }}>About</Link>
             <Link to="/services" style={{ color: 'white', marginRight: '10px' }}>Services</Link>
