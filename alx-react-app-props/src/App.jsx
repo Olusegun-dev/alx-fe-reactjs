cfb1732709e03eabd71d7bd48ba3fdb2f6a5041d
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-11-10 17:45:09 +0100

    Add alx-react-app-props directory and added props to the components

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,30 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <>
+      <div>
+        <Header />
+        <UserContext.Provider value={userData}>
+            <ProfilePage />
+        </UserContext.Provider>
+        <MainContent />
+        <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
