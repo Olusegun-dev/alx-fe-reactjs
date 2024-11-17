57794989edec4cbdc9c6c3dc3edf17e7d37b22a1
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-11-17 16:38:57 +0100

    Update DeleteRecipeButton

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,7 +1,9 @@
 import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate;
 
   return (
     <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>
