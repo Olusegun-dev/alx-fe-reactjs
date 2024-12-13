af6caeca8026a24f7eedbf6061c302e50e055866
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-12-13 18:27:17 +0100

    initial commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        <p>Search for GitHub profiles here!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
