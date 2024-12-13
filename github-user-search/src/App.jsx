3fa2d28c66a811f476d286d35dbb098ffff5746d
Olusegun Adeyemi <adeyemitemitayo09@gmail.com>
2024-12-13 18:33:02 +0100

    Add SearchInput component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchInput from "./components/SearchInput"
 
 function App() {
   return (
---
+++ github-user-search/src/components/SearchInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchInput() {
+  return (
+    <div>
+      <input type="text" placeholder="Search GitHub profiles..." />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchInput;
